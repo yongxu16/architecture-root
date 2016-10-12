941b3e87ed94b98f76113d84fd6cac7a5f3e28c1
yongxu16 <yongxu16@163.com>
2016-10-12 10:08:04 +0800

    add cart model

--- cartmgr/src/main/java/org/agile4j/architecture/cartmgr/service/CartService.java
+++ cartmgr/src/main/java/org/agile4j/architecture/cartmgr/service/CartService.java
@@ -5,7 +5,9 @@ import org.agile4j.architecture.cartmgr.vo.CartModel;
 import org.agile4j.architecture.cartmgr.vo.CartQueryModel;
 import org.agile4j.common.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartService extends BaseService<CartModel, CartQueryModel> implements ICartService {
 
 	private CartDAO cartDAO ;
--- cartmgr/src/main/java/org/agile4j/architecture/cartmgr/web/CartController.java
+++ cartmgr/src/main/java/org/agile4j/architecture/cartmgr/web/CartController.java
@@ -2,8 +2,12 @@ package org.agile4j.architecture.cartmgr.web;
 
 import org.agile4j.architecture.cartmgr.service.ICartService;
 import org.agile4j.architecture.cartmgr.vo.CartModel;
+import org.agile4j.architecture.cartmgr.vo.CartQueryModel;
+import org.agile4j.common.utils.JsonUtil;
+import org.agile4j.common.utils.page.Page;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,12 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.common.base.Strings;
+
 @Controller
 @RequestMapping("/cart")
 public class CartController {
 
 	private static final Logger LOGGER = LogManager.getLogger(CartController.class) ;
 	
+	@Autowired
 	private ICartService cartService ;
 	
 	/**
@@ -77,10 +84,49 @@ public class CartController {
 		return "cart/cartUpdate";
 	}
 	
+	/**
+	 * 执行更新
+	 * @param cm
+	 * @return
+	 */
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public String update(@ModelAttribute("cm") CartModel cm) {
 		this.cartService.update(cm);
 		return "success" ;
+	}
+	
+	/**
+	 * 跳转到分页查询
+	 * @param wm
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="toList", method=RequestMethod.GET)
+	public String toList(@ModelAttribute("wm")CartWebModel wm,Model model){
+		CartQueryModel cqm = null ;
+		if (Strings.isNullOrEmpty(wm.getQueryJsonStr()) ||Strings.isNullOrEmpty(wm.getQueryJsonStr().trim())) {
+			cqm = new CartQueryModel() ;
+		} else {
+			cqm = JsonUtil.str2Object(wm.getQueryJsonStr(), CartQueryModel.class) ;
+		}
+		cqm.getPage().setNowPage(wm.getNowPage()); // wm的nowPage是前台传过来的最新的
+		if (wm.getRowCount() > 0) {
+			cqm.getPage().setRowCount(wm.getRowCount());// 取得baseWebModel的默认行数
+		}
+		LOGGER.debug(wm);
+		Page<CartModel> page = this.cartService.getByConditionPage(cqm) ;
+		model.addAttribute("page", page) ;
+		model.addAttribute("wm",wm);
+		return "cart/CartList";
+	}
+	
+	/**
+	 * 跳转到查询页面
+	 * @return
+	 */
+	@RequestMapping(value="toQuery", method=RequestMethod.GET)
+	public String tuQuery() {
+		return "cart/cartQuery";
 	}
 	
 }
